ff125505f57ca7b5fb1cafd178cb834821f4d279
Playing - pokemon go Xbox one edition <linkolnkrauskopf@gmail.com>
2026-03-14 20:44:47 -0600

    implemented next and prev month buttons with updating month grids to match amount of days for that month

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,30 +1,55 @@
+"use client";
 
+import { Button } from "@/components/ui/button";
 import { 
   startOfMonth, 
   endOfMonth, 
 
-  eachDayOfInterval 
+  eachDayOfInterval, 
+  subMonths,
+  addMonths,
+  format
 } from "date-fns";
+import { useState } from "react";
 
 const Dashboard = () => {
 
-const today = new Date();
+  const today = new Date();
+  const [viewingDate, setViewingDate] = useState(new Date());
 
-// 1. Get the start and end of the grid (including padding for the week)
-const firstDayOfMonth = startOfMonth(today); // The 1st
-const lastDayOfMonth = endOfMonth(today);
+  // 1. Get the start and end of the grid (including padding for the week)
+  const firstDayOfMonth = startOfMonth(viewingDate); // The 1st
+  const lastDayOfMonth = endOfMonth(viewingDate);
 
-// 2. This gives you an array of every day to render in the grid
-const calendarDays = eachDayOfInterval({ start: firstDayOfMonth, end: lastDayOfMonth });
+
+
+  const nextMonth = () => setViewingDate(addMonths(viewingDate, 1));
+  const prevMonth = () => setViewingDate(subMonths(viewingDate, 1));
+
+
+
+  // 2. This gives you an array of every day to render in the grid
+  const calendarDays = eachDayOfInterval({ start: firstDayOfMonth, end: lastDayOfMonth });
 
 //=> "3 days ago"
 
   return (
     <main>
-      <div></div>
-      <div className="grid grid-cols-7 gap-2">
+
+      <div className="flex-col text-2xl justify-center items-center p-10">
+        <div className="flex justify-center items-center gap-4">
+          <h2>{format(viewingDate, 'MMMM yyyy')}</h2>
+          
+        </div>
+        <div className="flex justify-center items-center gap-4 mt-4">
+          <Button onClick={prevMonth} className="cursor-pointer text-2xl">Previous Month</Button>
+          <Button onClick={nextMonth} className="cursor-pointer text-2xl">Next Month</Button>
+        </div>
+
+      </div>
+      <div className="grid grid-cols-7 gap-2 p-30">
         {calendarDays.map((day) => (
-          <div key={day.toString()} className="border h-32 w-full p-2">
+          <div key={day.toString()} className="border h-60 w-full p-2">
             {day.getDate()}
             {/* This is where you would drop your Sticky Notes! */}
           </div>
