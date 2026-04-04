8ade4229525409977e85a5c64ab18ff6ddaa38b8
Playing - pokemon go Xbox one edition <linkolnkrauskopf@gmail.com>
2026-04-04 17:09:34 -0600

    added text to explain the product and added get started button to landing page, also a redirect button on sign up page to go back to landing page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -4,6 +4,7 @@
 import { sendEmail } from "@/app/actions";
 import { authClient } from "@/lib/auth-client";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 import { useForm } from "react-hook-form";
@@ -58,35 +59,46 @@ const SignUp = () => {
 
 
   return (
-    <div className="w-full h-lvh flex bg-blue-50 items-center justify-center flex-col">
-      <p className="text-4xl font-bold text-black mb-5">Sign up</p>
-      <div className="w-180 h-250 flex  justify-center items-center bg-white rounded-2xl border-6 border-gray-100 shadow-lg flex-col">
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col  w-full h-full items-center p-10">
-          <p className="font-inter text-3xl font-bold mb-25">Notely</p>
-          <label className="font-inter text-4sm items-start w-full text-black">Name</label>
-          <input type="text" placeholder="Name" {...register("name")} className="border-2 border-gray-300 h-12 w-full rounded-sm pl-2 font-inter"/>
-          {errors.name && <p>{errors.name.message}</p>}
+    <div className=" bg-blue-50">
+      <div >
+        <div className="w-full h-20 flex items-center justify-start pl-10 position: fixed">
+          <button>
+            <Link href="/" className="text-4xl font-inter font-bold text-blue-700">Notely</Link>
+          </button>
+        </div>
+        <div className="w-full h-lvh flex bg-blue-50 items-center justify-center flex-col">
+          <p className="text-4xl font-bold text-black mb-5">Sign up</p>
+          <div className="w-180 h-250 flex  justify-center items-center bg-white rounded-2xl border-6 border-gray-100 shadow-lg flex-col">
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col  w-full h-full items-center p-10">
+              <p className="font-inter text-3xl font-bold mb-25">Notely</p>
+              <label className="font-inter text-4sm items-start w-full text-black">Name</label>
+              <input type="text" placeholder="Name" {...register("name")} className="border-2 border-gray-300 h-12 w-full rounded-sm pl-2 font-inter"/>
+              {errors.name && <p>{errors.name.message}</p>}
 
-          <label className="font-inter text-4sm items-start w-full mt-4 text-black">E-mail</label>
-          <input type="text" placeholder="Email" {...register("email")} className="border-2 border-gray-300 h-12 w-full rounded-sm pl-2 font-inter"/>
-          {errors.email && <p>{errors.email.message}</p>}
+              <label className="font-inter text-4sm items-start w-full mt-4 text-black">E-mail</label>
+              <input type="text" placeholder="Email" {...register("email")} className="border-2 border-gray-300 h-12 w-full rounded-sm pl-2 font-inter"/>
+              {errors.email && <p>{errors.email.message}</p>}
 
-          <label className="font-inter text-4sm items-start w-full mt-4 text-black">Password</label>
-          <input type="password" placeholder="Password" {...register("password")} className="border-2 border-gray-300 h-12 w-full rounded-sm pl-2 font-inter"  />
-          {errors.password && <p>{errors.password.message}</p>}
+              <label className="font-inter text-4sm items-start w-full mt-4 text-black">Password</label>
+              <input type="password" placeholder="Password" {...register("password")} className="border-2 border-gray-300 h-12 w-full rounded-sm pl-2 font-inter"  />
+              {errors.password && <p>{errors.password.message}</p>}
 
-          <label className="font-inter text-4sm items-start w-full mt-4 text-black">Confirm Password</label>
-          <input type="password" placeholder="Confirm Password" {...register("confirmPassword")} className="border-2 border-gray-300 h-12 w-full rounded-sm pl-2 font-inter"/>
-          {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+              <label className="font-inter text-4sm items-start w-full mt-4 text-black">Confirm Password</label>
+              <input type="password" placeholder="Confirm Password" {...register("confirmPassword")} className="border-2 border-gray-300 h-12 w-full rounded-sm pl-2 font-inter"/>
+              {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
 
-          <button type="submit" disabled={isSubmitting} className="font-inter text-2xl cursor-pointer p-3 w-full bg-blue-700 text-white rounded-sm mt-10">Sign Up</button>
+              <button type="submit" disabled={isSubmitting} className="font-inter text-2xl cursor-pointer p-3 w-full bg-blue-700 text-white rounded-sm mt-10">Sign Up</button>
 
-          <p className="font-inter text-3sm mt-4">Already have an account? <a href="/sign-in" className="text-blue-700 hover:underline">Sign In</a></p>
+              <p className="font-inter text-3sm mt-4">Already have an account? <a href="/sign-in" className="text-blue-700 hover:underline">Sign In</a></p>
 
-        <p className="mt-8">or</p>
+            <p className="mt-8">or</p>
 
-        <button className="font-inter text-2xl cursor-pointer p-3 w-full border-2 border-gray-300 text-black rounded-sm mt-4">Sign up with Google</button>
-        </form>
+            <button className="font-inter text-2xl cursor-pointer p-3 w-full border-2 border-gray-300 text-black rounded-sm mt-4">Sign up with Google</button>
+            </form>
+
+        </div>
+
+        </div>
 
       </div>
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,37 @@
 export default function Home() {
   return (
     <div >
-      <div>
-        <header className="p-4 flex flex-row w-full justify-between items-center">
+      <div >
+        <header className="p-4 flex flex-row w-full justify-between items-center mt-2 position: fixed">
           <div>
-            <h1 className="text-2xl font-bold font-inter justify-between items-center">Welcome to Notely</h1>
+            <h1 className="text-4xl font-inter justify-between items-center ml-6">Notely</h1>
 
           </div>
-          <div className="flex flex-row  items-center bg-blue-600 text-l rounded-sm">
+          <div className="gap-10 flex text-xl items-center justify-center">
+            <button className="cursor-pointer font-inter p-2">Product</button>
+            <button className="cursor-pointer font-inter p-2">Features</button>
+            <button className="cursor-pointer font-inter p-2">Pricing</button>
+          </div>
+          <div className="flex flex-row  items-center bg-blue-600 text-l rounded-sm mr-10">
             <a href="/sign-up" className="w-full h-full cursor-pointer p-2 pl-6 pr-6 text-white font-inter">Get Started</a>
           </div>
         </header>
+      </div>
+      <div className="w-full h-dvh flex  justify-start pl-20">
+        <div className="w-2/4 h-full flex flex-col justify-center items-start pb-50">
+          <h1 className="font-inter font-bold text-5xl/20  font-stretch-expanded">
+            Project Management Software For <br />Superintendents, Foremen, And Project Managers.
+          </h1>
+          <h5 className="font-inter font-stretch-expanded pr-5 mt-5 text-xl text-gray-600">
+            Pull Planning for superintendents, foremen, and project managers. Notely is a construction project management software that helps you plan, track and manage your projects more efficiently.
+          </h5>
+          <div className="flex w-full items-center justify-start pt-4">
+            <button className="text-lg/15 ">
+              <a href="/sign-up" className=" cursor-pointer text-white font-inter bg-blue-600 rounded-sm pl-5 pr-5 p-3">Get Started for Free</a>
+            </button>
+
+          </div>
+        </div>
       </div>
     </div>
   );
